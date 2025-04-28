bd0f82a3ad7c3b0e9afb393bc8abaf5090edec2a
Ewerton Igor <ewertonigor@icloud.com>
2025-04-28 01:57:02 -0300

    refactor: authOption and fonts next

---
+++ src/lib/authOptions.ts
@@ -0,0 +1,55 @@
+import { type NextAuthOptions } from 'next-auth'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '@/lib/prisma'
+import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import bcrypt from 'bcryptjs'
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Email e senha são obrigatórios')
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        })
+
+        if (!user || !user.password) {
+          throw new Error('Usuário não encontrado')
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.password,
+        )
+
+        if (!isCorrectPassword) {
+          throw new Error('Senha incorreta')
+        }
+
+        return user
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/sign-in',
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+}
