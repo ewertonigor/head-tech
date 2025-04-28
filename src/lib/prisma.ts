c24a12948731e06c4435b55830958991530276fb
Ewerton Igor <ewertonigor@icloud.com>
2025-04-28 01:57:02 -0300

    fix: configuration prisma cliente

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,13 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@/generated/prisma'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient
 }
+
+const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
